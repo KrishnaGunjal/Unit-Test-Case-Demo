06136b1b006ba8a782207e01a944307f58177953
Krishna Gunjal <kgunjal51@gmail.com>
2022-06-02 10:15:49 +0530

    Initial commit

---
+++ UnitTestCaseDemo/UnitTestCaseDemo/UnitTestCaseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestCaseDemoApp.swift
+//  UnitTestCaseDemo
+//
+//  Created by Krishna Gunjal on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestCaseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
